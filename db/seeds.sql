a1255daf3eda78db61d57b4663ec4445deaa850f
Calvin <Calvin.d.Bowman@gmail.com>
2018-08-12 21:52:32 -0400

    trying to get this to work

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Goat Cheese burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Avocado Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Southern Style Turkey Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chicken Chilli Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Heavy Burger', true);
